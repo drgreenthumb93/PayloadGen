f7c76910dfef332dd6e02b64304133d273adcdab
[0xZ3T] <50826299+drgreenthumb93@users.noreply.github.com>
2022-05-10 14:44:43 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -252,7 +252,7 @@ def Banner():
 (  _ \ /__\ ( \/ )/ __)( ___)( \( )
  )___//(__)\ \  /( (_-. )__)  )  ( 
 (__) (__)(__)(__) \___/(____)(_)\_)
-Coded By youhacker55
+Coded by DrGreenthumb93
 
 Don t upload Payload To VirusTotal 
   """)
